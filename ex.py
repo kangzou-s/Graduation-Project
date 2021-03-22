17a97f988596fe72d72a0c115a7b9431e6718872
清欢 <qinghuan@qinghuandeMacBook-Pro.local>
2021-03-22 17:02:02 +0100

    some results

--- ex.py
+++ ex.py
@@ -5,25 +5,27 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt #import function library which will be used 
 
+#part1 Pretreatment
 pattern = cv.imread('./puzzle_generator/puzzle_unsolved.jpg')
 pre = cv.imread('./puzzle_generator/puzzle_unsolved.jpg')
 
-
+#Extract background
 pattern = cv.cvtColor(pattern,cv.COLOR_BGR2HSV)
-
 lower_green = np.array([45,50,50])
 upper_green = np.array([81,255,255])
 mask1 = cv.inRange(pattern,lower_green,upper_green)
 res = cv.bitwise_and(pattern,pattern, mask= mask1)
 pattern = cv.cvtColor(res,cv.COLOR_HSV2BGR)
 
+#change to binary image and make filter
 pattern = (0.3*pattern[:,:,2] + 0.6*pattern[:,:,1] + 0.1*pattern[:,:,0]).astype(np.uint8)
 pattern = 255 * (pattern < 142).astype(np.uint8)
 
-
-'''cv.imshow('',pattern)
-cv.waitKey(0)'''
-
+kernel = np.ones((5,5),np.uint8)
+#pattern = cv.morphologyEx(pattern,cv.MORPH_OPEN,kernel)
+kernel2 = np.ones((3,3),np.uint8)
+pattern = cv.erode(pattern,kernel,iterations = 1)
+pattern = cv.dilate(pattern,kernel2,iterations = 1)
 #part 2 find contour
 
 contours, _ = cv.findContours(pattern, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -35,9 +37,7 @@ print('Contour lengths: ', [len(i) for i in contours])
 res = np.ones(pattern.shape, dtype=np.uint8) * 255
 cv.drawContours(res, contours, -1, (100,100,100), thickness=cv.FILLED)
 
-'''cv.imshow('',res)
-cv.waitKey(0)'''
-
+#cut pieces
 pieces = []
 for i in range(len(contours)):
 	mask = np.ones(pattern.shape, dtype=np.uint8)*255
@@ -46,44 +46,19 @@ for i in range(len(contours)):
 
 	piece = copy.deepcopy(pre)
 	piece[mask > 0] = 0
-	rows,cols=piece.shape[:2]
-
-	
-	'''cv.imshow('',piece)
-	cv.waitKey(0)'''
 	
 	x,y,w,h = cv.boundingRect(contours[i])
 	print(x, y, w, h)
-	a = piece[y:y+h, x:x+w]
-	
-	cv.imshow('',a)
+	pieces.append(piece[y:y+h, x:x+w])
+
+	cv.imshow('',piece)
 	cv.waitKey(0)
 
-
-	rect = cv.minAreaRect(contours[i])
-	x,y = np.int0(rect[0])
-	w,h = np.int0(rect[1])
-	M = cv.getRotationMatrix2D((x,y),rect[2],1)
-	piece = cv.warpAffine(a,M,(cols,rows))
-
-
-	'''cv.imshow('',piece)
-	cv.waitKey(0)'''
-
-	
-	
-	
-
-
-
-	x,y,w,h = cv.boundingRect(contours[i])
-	#print(x, y, w, h)
-	pieces.append(piece[y:y+h, x:x+w])
 
 side = np.ceil(np.sqrt(len(pieces)))
 for i in range(len(pieces)):
 	plt.subplot(side, side, i+1), plt.imshow(pieces[i])
-	#cv.imwrite('piece_'+str(i)+'.png', pieces[i])
+	cv.imwrite('./segmented/piece_'+str(i)+'.png', pieces[i])
 	
 plt.show()
 
