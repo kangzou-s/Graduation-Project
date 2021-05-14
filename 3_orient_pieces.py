e9988d790dab64a17a397324357bb045f40f9754
kangzou-s <79980826+kangzou-s@users.noreply.github.com>
2021-05-14 10:46:59 +0200

    Delete 3_orient_pieces.py

--- 3_orient_pieces.py
+++
@@ -1,3 +0,0 @@
-# This script should take an individual structurized object of class Piece,
-# orients (rotates and centers) it so that its image is not skewed and saves
-# as an object to the disk in './results' folder.
